1924c334c99d5134c8bb545faaf8d275d3259535
kienpachi <mohamed-elhady@outlook.com>
2017-07-10 18:14:25 +0200

    open/close filter menu, edits in CSS

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    
+        // open/close filteration menu
+        $('#menu-icon').click(function () {
+            $('.locations').toggleClass('collapse');
+        });
+    
+    
+    
+});
